7083cdfd04598a37fe451e61f9aa64cb532c954d
Rakesh Kumar <rakeshkumar92215@gmail.com>
2019-10-19 18:24:00 -0400

    new class

---
+++ C0764930_F2019_MID_TERM_MAD3004/MyMobileBill.swift
@@ -0,0 +1,39 @@
+//
+//  MyMobileBill.swift
+//  C0764930_F2019_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2019-10-19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+class MyMobileBill : MyBill{
+
+
+    var manufacturer_Name:String
+    var plan:String
+    var Phone_Number:String
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
