5bbdcad47fd29fdf3ea05de08227a200ebe6c770
Rakesh Kumar <rakeshkumar92215@gmail.com>
2019-10-19 18:50:23 -0400

    class created

---
+++ C0764930_F2019_MID_TERM_MAD3004/MyHydroBill.swift
@@ -0,0 +1,24 @@
+//
+//  MyHydroBill.swift
+//  C0764930_F2019_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2019-10-19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+class MyHydroBill : MyBill
+{
+
+    var agency_Name:String
+    var units_Used: Float
+
+
+
+
+
+
+
+
+
+}
