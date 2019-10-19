73fe5a5ececd364e629a2cbc2cdfa944fa5016e6
Rakesh Kumar <rakeshkumar92215@gmail.com>
2019-10-19 19:21:34 -0400

    new class insurance

---
+++ C0764930_F2019_MID_TERM_MAD3004/MyInsuranceBill.swift
@@ -0,0 +1,25 @@
+//
+//  MyInsuranceBill.swift
+//  C0764930_F2019_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2019-10-19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+class MyInsuranceBill:MyBill
+{
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
